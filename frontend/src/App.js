d903eb0e6edd63c468890d08dc86dcfb79430bfa
Saarthak Mudigere Girish <saarthakmudigere@gmail.com>
2024-03-02 13:50:46 -0600

    Commiting Basic content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Home from "./Components/Home.js";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -23,10 +18,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/project" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="*" element={<Navigate to="/"/>} /> */}
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
         </Routes>
       </div>
     </Router>
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -2,43 +2,105 @@ import React from "react";
 
 function Home() {
   return (
-    <h1>This is Saarthak!</h1>
-    // <section>
-    //   <Container fluid className="home-section" id="home">
-    //     <Particle />
-    //     <Container className="home-content">
-    //       <Row>
-    //         <Col md={7} className="home-header">
-    //           <h1 style={{ paddingBottom: 15 }} className="heading">
-    //             Hi There!{" "}
-    //             <span className="wave" role="img" aria-labelledby="wave">
-    //               👋🏻
-    //             </span>
-    //           </h1>
+    <div>
+      {/* <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Saarthak Mudigere Girish</title> */}
 
-    //           <h1 className="heading-name">
-    //             I'M
-    //             <strong className="main-name"> SAARTHAK MUDIGERE GIRISH</strong>
-    //           </h1>
+      {/* <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl7/1L_dstPt3HV5HzF6Gvk/e3s4Wz6iJgD/+ub2oU"
+        crossorigin="anonymous"
+      /> */}
 
-    //           <div style={{ padding: 50, textAlign: "left" }}>
-    //             <Type />
-    //           </div>
-    //         </Col>
+      {/* <header class="bg-primary text-white p-3">
+        <div class="container">
+          <div class="d-flex justify-content-between align-items-center">
+            <h1>Saarthak Mudigere Girish</h1>
+            <a href="mailto:youremail@example.com" class="btn btn-light">
+              Contact Me
+            </a>
+          </div>
+        </div>
+      </header> */}
 
-    //         <Col md={5} style={{ paddingBottom: 20 }}>
-    //           <img
-    //             src={homeLogo}
-    //             alt="home pic"
-    //             className="img-fluid"
-    //             style={{ maxHeight: "450px" }}
-    //           />
-    //         </Col>
-    //       </Row>
-    //     </Container>
-    //   </Container>
-    //   <Home2 />
-    // </section>
+      <main class="container py-4">
+        <section id="introduction">
+          <h2>Hello, I'm Saarthak Mudigere Girish</h2>
+          <p>
+            A Computer Science graduate with a keen interest in Full Stack
+            Development and DevOps, known for relentless passion for learning
+            and a collaborative team spirit.
+          </p>
+        </section>
+        <section id="aboutme">
+          <h2>About me</h2>
+          <p>
+            I'm currently a Computer Science Master's student at the University
+            of Texas at Arlington (UTA), driven by an unwavering passion for
+            technology and innovation. My journey began when I embarked on a Web
+            Developer Internship, which served as a catalyst for my deep
+            fascination with the boundless opportunities offered by the field of
+            computer science. Applying my skills to create impactful projects
+            that push the limits of what is possible gives me a great deal of
+            joy.
+          </p>
+          <p>
+            Throughout my carreer, I've meticulously cultivated my expertise
+            across diverse domains, including Full Stack Web Development,
+            DevOps, and Machine Learning. My commitment to knowledge expansion
+            is unwavering, as I continually seek opportunities to broaden my
+            horizons and deepen my proficiency in these areas. As I embark on
+            the next chapter of my career, I am enthusiastic about applying my
+            well-rounded skill set to real-world challenges and driving
+            innovation in the world of technology.
+          </p>
+        </section>
+
+        <section id="experience">
+          <h2>Experience</h2>
+          <h3>Web Developer Internship</h3>
+          <p>
+            My journey began when I embarked on a Web Developer Internship,
+            which served as a catalyst for my deep fascination with the
+            boundless opportunities offered by the field of computer science.
+          </p>
+        </section>
+
+        <section id="skills">
+          <h2>Skills</h2>
+          <ul>
+            <li>Full Stack Web Development</li>
+            <li>DevOps</li>
+            <li>Machine Learning</li>
+          </ul>
+        </section>
+
+        <section id="education">
+          <h2>Education</h2>
+          <p>
+            Currently pursuing a Master's degree in Computer Science at the
+            University of Texas at Arlington (UTA).
+          </p>
+        </section>
+
+        <section id="projects">
+          <h2>Projects</h2>
+        </section>
+      </main>
+
+      <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz4fnFO9gybBud7RduPuemT//+jJXB16zg6i8UQD3lV5uDC3Yc7bz1Eeow"
+        crossorigin="anonymous"
+      ></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/"
+        crossorigin="anonymous"
+      ></script>
+    </div>
   );
 }
 
