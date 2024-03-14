ee27172a9f6d276b02c1aa0f6a40019af46a591d
Saarthak Mudigere Girish <saarthakmudigere@gmail.com>
2024-03-14 18:38:00 -0500

    Installing Tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,18 +20,22 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
   return (
-    <Router>
-      <div className="App">
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/project" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="*" element={<Navigate to="/"/>} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      {/* <NavigationBar /> */}
+      <Home />
+    </div>
+    // <Router>
+    //   <div className="App">
+    //     <NavigationBar />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       {/* <Route path="/project" element={<Projects />} />
+    //         <Route path="/about" element={<About />} />
+    //         <Route path="/resume" element={<Resume />} />
+    //         <Route path="*" element={<Navigate to="/"/>} /> */}
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 }
 
