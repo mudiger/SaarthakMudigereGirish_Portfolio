e2b9068ec9f052c1a35e7b01a0ec1a6c6b48cbef
Saarthak Mudigere Girish <saarthakmudigere@gmail.com>
2024-03-16 17:26:59 -0500

    Deleted Layout.js and Done with basic app, about to start with hook.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-import Layout from "./Layout.js";
-import Home from "./Components/Home.js";
 import Header from "./Components/Header.js";
-// import NavigationBar from "./Components/NavBar.js";
-// import "./App.css";
+import Footer from "./Components/Footer.js";
+import Home from "./Components/Home.js";
+import "./input.css";
 
 function App() {
   return (
-    // <div className="App">
     <div>
-      {/* <NavigationBar /> */}
-      <Layout>
-        <Home />
-      </Layout>
+      <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+      <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] x1:left-[-15rem] 2xl:left-[-5rem]"></div>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -4,9 +4,9 @@ import About from "./HomeComponents/about.js";
 
 function Home() {
   return (
-    // className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium sm:w-[initial] sm:flex-nowrap sm:gap-5"
-    <div classname="mb-10 px-18 text-center text-gray-500">
+    <div className="flex flex-col items-center justify-center px-4 h-screen">
       <Intro />
+      <p className="mb-20"></p>
       <About />
     </div>
   );
